4aafa5f43e83a43cc8563bf8909d6e2b98a31e48
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-06 18:54:35 -0600

    updated protect middleware
    updated protect middleware

--- src/middlewares/protect.middleware.js
+++ src/middlewares/protect.middleware.js
@@ -1,7 +1,19 @@
-function protect(req, res, next) {
-    console.log('secret!!')
+const jwt = require('jsonwebtoken')
 
-    next();
+function protect(req, res, next) {
+  try {
+    const token = req.headers.authorization
+
+    if (!token) {
+      return res.status(401).json({ error: 'Acceso denegado' })
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY)
+
+    next()
+  } catch (error) {
+    return res.status(400).json({ error: 'Token no valido' })
+  }
 }
 
 module.exports = protect
