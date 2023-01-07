5e9e2494fc06a97a9b59c3fc2556f276a40cb44c
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-07 12:56:54 -0600

    updated config server
    updated config server

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,6 +6,7 @@ const dbHost = process.env.DB_HOST || ''
 const dbPort = Number(process.env.DB_PORT) || 5432
 const dbName = process.env.DB_NAME || ''
 const PORT = Number(process.env.PORT) || 3000
+const accessUrl = process.env.ACCESS_URL || '*'
 const secretKey =
   process.env.SECRET_KEY || '402586e2faa8da4c98a35f1b20d6b033c60'
 const urlApiWeather = 'http://api.weatherapi.com/v1/current.json'
@@ -25,6 +26,7 @@ module.exports = {
   dbPort,
   dbName,
   PORT,
+  accessUrl,
   secretKey,
   urlApiWeather,
   apiWeatherKey,
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const compression = require('compression')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const app = express()
 const routers = require('../routers')
+const { accessUrl } = require('../constants')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(function (_, res, next) {
+    res.header("Access-Control-Allow-Origin", accessUrl)
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+    next()
+})
+app.use(helmet.hidePoweredBy())
 app.use(compression())
 app.use(morgan('combined'))
 
