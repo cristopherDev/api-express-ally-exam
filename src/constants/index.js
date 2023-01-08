a6e491830bd69ced14a632bd1f0eb87c229fda31
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-07 23:11:19 -0600

    config fly
    config fly

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASSWORD || ''
 const dbHost = process.env.DB_HOST || ''
 const dbPort = Number(process.env.DB_PORT) || 5432
 const dbName = process.env.DB_NAME || ''
-const PORT = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 8080
 const accessUrl = process.env.ACCESS_URL || '*'
 const secretKey =
   process.env.SECRET_KEY || '402586e2faa8da4c98a35f1b20d6b033c60'
